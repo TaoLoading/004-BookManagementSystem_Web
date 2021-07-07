19860263b12c1d91139ac3f3a2077fc67a7fa45a
TaoLoading <1377226380@qq.com>
2021-07-07 15:43:53 +0800

    权限控制

---
+++ src/views/book/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
